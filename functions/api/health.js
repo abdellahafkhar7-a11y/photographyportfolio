281f335b1c3de4786803a9ccfa01722277c34f42
abdellahafkhar7-a11y <abdellahafkhar7@gmail.com>
2026-07-02 21:40:22 +0100

    8

--- functions/api/health.js
+++
@@ -1,8 +0,0 @@
-import { success } from '../lib/response.js';
-
-export async function onRequest() {
-  return success({
-    message: 'Photography Pixel API is running',
-    timestamp: new Date().toISOString()
-  });
-}
